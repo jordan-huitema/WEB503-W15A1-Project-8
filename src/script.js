1541e0a0849f229101db6ed02a7d9d96d8f3d648
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-11-10 11:34:37 +1300

    managed to merge local and remote repositorys mannually after 20min of errors

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// DOM selectors
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
+
+const apiURL = 'https://api.lyrics.ovh'
+
+async function searchSongs(term) {
+
+}
